0ccba0b5a673e347673fff2a0085df6772d4e359
siva goutham <gouthamklu@gmail.com>
2020-03-25 15:50:50 +0530

    Updates

---
+++ precache-manifest.71fbdae51ce58e57859043afec8d413a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaef947585a166fc3c10cdadb88f96cf",
+    "url": "/reactLogin/index.html"
+  },
+  {
+    "revision": "8412a0abfb33d80bea21",
+    "url": "/reactLogin/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10ee78c6bf0bc0af8fb6",
+    "url": "/reactLogin/static/js/2.c5a010af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactLogin/static/js/2.c5a010af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8412a0abfb33d80bea21",
+    "url": "/reactLogin/static/js/main.26737d27.chunk.js"
+  },
+  {
+    "revision": "7fca450483dd3e64d52b",
+    "url": "/reactLogin/static/js/runtime-main.b14631b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactLogin/static/media/logo.5d5d9eef.svg"
+  }
+]);
