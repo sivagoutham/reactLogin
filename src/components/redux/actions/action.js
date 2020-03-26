8133ddeec2aab5830e625e3b47a71287240b02b8
siva goutham <gouthamklu@gmail.com>
2020-03-26 17:18:50 +0530

    firstcommit4

--- src/components/redux/actions/action.js
+++ src/components/redux/actions/action.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const getEmployees = ()=>{
     return dispatch =>{
-        return axios.get('https://github.com/sivagoutham/reactLogin/blob/master/public/dashboard.json').then((response)=>{
+        return axios.get('https://sivagoutham.github.io/reactLogin/dashboard.json/').then((response)=>{
             console.log(response);
             dispatch({type:GET_EMPLOYEES,payload:response.data})
         })
