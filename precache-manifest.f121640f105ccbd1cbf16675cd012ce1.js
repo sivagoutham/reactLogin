61461f3b9b86e6b1d96dd792462c93a46f408d4f
siva goutham <gouthamklu@gmail.com>
2020-03-28 13:22:07 +0530

    Updates

---
+++ precache-manifest.f121640f105ccbd1cbf16675cd012ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb812a695ccb6cdcd1448347783ea8b6",
+    "url": "/reactLogin/index.html"
+  },
+  {
+    "revision": "d685313559bad7ac3518",
+    "url": "/reactLogin/static/css/main.e7ea959d.chunk.css"
+  },
+  {
+    "revision": "9db68006f3521790e4d6",
+    "url": "/reactLogin/static/js/2.5a70815d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactLogin/static/js/2.5a70815d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d685313559bad7ac3518",
+    "url": "/reactLogin/static/js/main.aba36ea7.chunk.js"
+  },
+  {
+    "revision": "7fca450483dd3e64d52b",
+    "url": "/reactLogin/static/js/runtime-main.b14631b3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94f5b121fe4c0f492b8f5cc4feb01ba8.js"
+  "/reactLogin/precache-manifest.f121640f105ccbd1cbf16675cd012ce1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactLogin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
