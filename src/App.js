1cb92d3e461e1aa4edf354947332ee645e89511b
siva goutham <gouthamklu@gmail.com>
2020-03-28 17:08:50 +0530

    firstcommit48

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { history } from './components/redux/history';
 import Login from './components/Login/login';
 import Dashboard from './components/Dashboard/dashboard'
 function App() {
   return (
     <div className="App">
       <div className='container'>
-        <Router >
+        <Router history={history}>
           <Switch>
             <Route exact path="/" component={Login} />
             <Route path='/dashboard' component ={Dashboard}/>
